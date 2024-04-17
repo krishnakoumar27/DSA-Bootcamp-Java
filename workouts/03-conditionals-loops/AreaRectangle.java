227092315f780223aa33f1768e14fdd499c1b3b8
krishnakoumar <krishnakoumar27@gmail.com>
2024-04-17 14:21:43 +0530

    Added comments to AreaRectangle.java

--- workouts/03-conditionals-loops/AreaRectangle.java
+++ workouts/03-conditionals-loops/AreaRectangle.java
@@ -7,8 +7,10 @@ public class AreaRectangle {
   }
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
+    // get the length
     System.out.println("Enter the length of rectangle: ");
     double l = sc.nextDouble();
+    // get the width or height
     System.out.println("Enter the height of rectangle: ");
     double h = sc.nextDouble();
     double area = areaOfRectangle(l, h);
